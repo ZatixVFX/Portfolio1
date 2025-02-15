0c8c733112c72261e2024b87b4826afd8c81b39b
Junaid Salie <jsalie165@gmail.com>
2025-02-15 02:56:48 +0200

    add vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,7 +12,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://mail-server-lyart.vercel.app/api",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
