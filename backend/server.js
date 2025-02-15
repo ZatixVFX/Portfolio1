7de293cfd7bcd7a63d50b6d009e7ccd0d4ed7b7f
Junaid Salie <jsalie165@gmail.com>
2025-02-15 02:22:53 +0200

    add vercel

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
   //   res.send("API is running....");
   // });
 } else {
-  app.get("/api", (req, res) => {
+  app.get("/", (req, res) => {
     res.send("API is running....");
   });
 }
