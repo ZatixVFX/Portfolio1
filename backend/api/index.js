807aeadc73a4d5897955748e4d2567c34ec90299
Junaid Salie <jsalie165@gmail.com>
2025-02-21 19:39:01 +0200

    cors fix

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,14 +12,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
-
-let allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
 };
-app.use(allowCrossDomain);
+
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
+app.use(allowCors(handler));
+app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
