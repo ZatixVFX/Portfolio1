3850d7879391307f49e6c1ff5edf9fb65d52326d
Junaid Salie <jsalie165@gmail.com>
2025-02-21 19:27:56 +0200

    cors fix

--- backend/api/index.js
+++ backend/api/index.js
@@ -14,6 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
+let allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+};
+app.use(allowCrossDomain);
+
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "frontend/dist")));
